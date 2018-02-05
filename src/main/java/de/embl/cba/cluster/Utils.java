bbb5f3dbcaa40ecb5c9226dd666bc1a583f56fc1
Christian Tischer <christian.tischer@embl.de>
2018-02-05 09:47:24 +0100

    nice

--- src/main/java/de/embl/cba/cluster/Utils.java
+++ src/main/java/de/embl/cba/cluster/Utils.java
@@ -109,4 +109,47 @@ public abstract class Utils
             os.close();
         }
     }
+
+
+    public static String getFullPathFromWindowsMappedDriveLetterPath( String mappedPath )
+    {
+        /*
+        public static String getOsName()
+        {
+            if(OS == null) { OS = System.getProperty("os.name"); }
+            return OS;
+        }
+        public static boolean isWindows()
+        {
+            return getOsName().startsWith("Windows");
+        }
+        */
+
+        Runtime runTime = Runtime.getRuntime();
+        Process process = null;
+        try
+        {
+            process = runTime.exec("net use");
+        }
+        catch ( IOException e )
+        {
+            e.printStackTrace();
+        }
+        InputStream inStream = process.getInputStream();
+        InputStreamReader inputStreamReader = new InputStreamReader(inStream);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+        String line = null;
+        String[] components = null;
+        String fullPath = null;
+        while (null != (line = bufferedReader.readLine())) {
+            components = line.split("\\s+");
+            if ((components.length > 2) && (components[1].equals(mappedPath.substring(0, 2)))) {
+                fullPath = mappedPath.replace(components[1], components[2]);
+            }
+        }
+
+        return fullPath;
+    }
+
+
 }
