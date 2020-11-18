963f308bc6af1d03d47ecd53d5a0304f2c2c7a81
Christian Tischer <christian.tischer@embl.de>
2020-11-18 15:26:00 +0100

    Prepare for release

--- src/main/java/de/embl/cba/cluster/dependencies/Dependency.java
+++ src/main/java/de/embl/cba/cluster/dependencies/Dependency.java
@@ -2,17 +2,14 @@ package de.embl.cba.cluster.dependencies;
 
 public class Dependency
 {
-
     public String key;
     public DependencyType localType;
     public DependencyType remoteType;
     public Object localObject;
     public Object remoteObject;
 
-
     public void setLocalSource( Object localObject )
     {
         this.localObject = localObject;
     }
-
 }
